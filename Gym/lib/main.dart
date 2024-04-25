26bb69f1652f8605db564cfc2bb089b6c6f2a4b1
youssefabdelhady12345 <157917927+youssefabdelhady12345@users.noreply.github.com>
2024-04-25 19:52:00 +0200

    Add files via upload

---
+++ Gym/lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp( Gym());
+}
+
+class Gym extends StatelessWidget {
+
+
+  @override
+  Widget build(BuildContext context) {
+    return MateaterialApp ();
+  }
+}
